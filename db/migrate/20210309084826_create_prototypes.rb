6375f5f6630715efc832fc15de3b4a8085fa349a
nakakou0703 <kknn070307@icloud.com>
2021-03-12 12:44:34 +0900

    修正３

--- db/migrate/20210309084826_create_prototypes.rb
+++ db/migrate/20210309084826_create_prototypes.rb
@@ -6,7 +6,7 @@ class CreatePrototypes < ActiveRecord::Migration[6.0]
       t.string :title,        null: false
       t.text :catch_copy,     null: false
       t.text :concept,        null: false
-      t.references :user,     null: false
+      t.references :user, foreign_key: true,   null: false
     end
   end
 end
